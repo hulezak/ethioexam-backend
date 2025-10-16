5a8db483f17f4a5b490b549c346f62384a74fb95
Haile Adane Assefa <109754116+hulezak@users.noreply.github.com>
2025-10-16 11:44:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',      // for local React dev
   'https://ethioexam.netlify.app',
-  'https://ethioexam2.netlify.app' // production React app
+  'https://ethioexam2.netlify.app',
+  'https://ethioexam.pro.et'// production React app
 ];
 
 app.use(cors({
@@ -1447,6 +1448,7 @@ const PORT = process.env.PORT || 3000;
   });
 })();
    
+
 
 
 
