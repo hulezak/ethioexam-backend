82a3096a9caebc7f361754bd6bf1bf3d0d71f23e
Haile Adane Assefa <109754116+hulezak@users.noreply.github.com>
2025-10-16 15:10:58 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',      // for local React dev
   'https://ethioexam.netlify.app',
-  'https://ethioexam2.netlify.app' // production React app
+  'https://ethioexam2.netlify.app' ,// production React app
   'https://ethioexam.pro.et' // production React app
 ];
 
