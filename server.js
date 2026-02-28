300690b49ed90e9be690db020afda797b517482c
Haile Adane Assefa <109754116+hulezak@users.noreply.github.com>
2026-02-28 08:04:25 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const allowedOrigins = [
   'http://localhost:5173',      // for local React dev
   'https://ethioexam.netlify.app',
   'https://ethioexam2.netlify.app' ,// production React app
-  'https://ethioexam.pro.et' // ,
+  'https://ethioexam.pro.et',
   'https://ethioexam.et'
 ];
 
@@ -2021,6 +2021,7 @@ const PORT = process.env.PORT || 3000;
   });
 })();
    
+
 
 
 
